c2c56b19fbe61269ac167b9ca37f193b2c71df0b
Gabriel <gmachicaoq@unsa.edu.pe>
2024-04-26 17:00:38 -0500

    Iniciando el proyecto MVC con Security

---
+++ src/main/java/pe/edu/cibertec/WAEC2Machicao_Gabriel/Waec2MachicaoGabrielApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Machicao_Gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2MachicaoGabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2MachicaoGabrielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/WAEC2Machicao_Gabriel/Waec2MachicaoGabrielApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Machicao_Gabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2MachicaoGabrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
