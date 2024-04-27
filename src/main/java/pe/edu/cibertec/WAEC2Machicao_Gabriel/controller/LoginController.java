78602bd0504272c6c5c3d6accb364cea97b40394
Gabriel <gmachicaoq@unsa.edu.pe>
2024-04-26 20:26:22 -0500

    Agregando el LoginController y colocando el bootstrap y templates necesarios

---
+++ src/main/java/pe/edu/cibertec/WAEC2Machicao_Gabriel/controller/LoginController.java
@@ -0,0 +1,51 @@
+package pe.edu.cibertec.WAEC2Machicao_Gabriel.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.cibertec.WAEC2Machicao_Gabriel.model.bd.Usuario;
+import pe.edu.cibertec.WAEC2Machicao_Gabriel.model.dto.security.UsuarioSecurity;
+import pe.edu.cibertec.WAEC2Machicao_Gabriel.service.IUsuarioService;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/auth")
+public class LoginController {
+    private IUsuarioService iUsuarioService;
+    @GetMapping("/login")
+    public String login(){
+        return "backoffice/auth/frmlogin";
+    }
+    @GetMapping("/login-success")
+    public String loginSuccess(){
+        return "redirect:/auth/dashboard";
+    }
+
+    @GetMapping("/registrar")
+    public String registrar(){
+        return "backoffice/auth/frmRegistroUsuario";
+    }
+
+    @PostMapping("/guardarUsuario")
+    public String guardarUsuario(@ModelAttribute Usuario usuario){
+        iUsuarioService.guardarUsuario(usuario);
+        return "backoffice/auth/frmLogin";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        UserDetails userDetails = (UserDetails) SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        UsuarioSecurity usuarioSecurity = (UsuarioSecurity) userDetails;
+        session.setAttribute("nomusuario", usuarioSecurity.getNombre());
+        return "backoffice/auth/home";
+    }
+}
